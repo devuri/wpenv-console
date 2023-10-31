ccc642189f3d915e8238c45589d2303b78ae56bb
devuri <devuri@users.noreply.github.com>
2023-10-31 12:55:19 -0500

    fix!: upgrade to 0.3 `devuri/encryption`

--- src/inc/helpers.php
+++ src/inc/helpers.php
@@ -12,28 +12,34 @@ if ( ! \function_exists( 'wpenv' ) ) {
      */
     function wpenv( string $name, $default = null, bool $strtolower = false )
     {
-        if ( ! isset( $_ENV[ $name ] ) ) {
-            return $default;
+		if ( isset( $_ENV[ $name ] ) ) {
+			$env_value = $_ENV[ $name ];
+        } else {
+			$env_value = null;
+		}
+
+		if( is_null( $env_value ) ){
+			return $default;
+		}
+
+        if ( is_numeric( $env_value ) && \intval( $_ENV[ $name ] ) ) {
+            return (int)  $env_value;
         }
 
-        if ( is_numeric( $_ENV[ $name ] ) && \intval( $_ENV[ $name ] ) ) {
-            return (int) $_ENV[ $name ];
-        }
-
-        if ( \in_array( $_ENV[ $name ], [ 'True', 'true', 'TRUE' ], true ) ) {
+        if ( \in_array( $env_value, [ 'True', 'true', 'TRUE' ], true ) ) {
             return true;
         }
-        if ( \in_array( $_ENV[ $name ], [ 'False', 'false', 'FALSE' ], true ) ) {
+        if ( \in_array( $env_value, [ 'False', 'false', 'FALSE' ], true ) ) {
             return false;
         }
-        if ( \in_array( $_ENV[ $name ], [ 'Null', 'null', 'NULL' ], true ) ) {
+        if ( \in_array( $env_value, [ 'Null', 'null', 'NULL' ], true ) ) {
             return '';
         }
 
         if ( $strtolower ) {
-            return strtolower( $_ENV[ $name ] );
+            return strtolower( $env_value );
         }
 
-        return $_ENV[ $name ];
+        return $env_value;
     }
 }// end if
